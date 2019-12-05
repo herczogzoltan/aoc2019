5fc6188e5d9fe3fb1c9596a887de75d194478cd7
Zoltán Herczog <zoltan.herczog@cheppers.com>
2019-12-06 00:08:49 +0100

    Add part 2

---
+++ cmd/day1.go
@@ -0,0 +1,89 @@
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"strconv"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(day1)
+}
+
+var (
+	day1 = &cobra.Command{
+		Use: "day1",
+		Run: func(cmd *cobra.Command, args []string) {
+			switch Part {
+			case 1:
+				fmt.Println("First part result: ", firstPart())
+			case 2:
+				fmt.Println("Second part result: ", secondPart())
+			}
+
+		},
+	}
+)
+
+func firstPart() int {
+	file := LoadSourceFile(InputSource)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var total int
+
+	for scanner.Scan() {
+		mass, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Error("An error occured while converting the line of the file from string to int")
+		}
+		total += calculateFuel(mass)
+	}
+
+	return total
+}
+
+func calculateFuel(mass int) int {
+	return int(math.Trunc(((float64(mass) / 3) - 2)))
+}
+
+func secondPart() int {
+	file := LoadSourceFile(InputSource)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var total int
+	for scanner.Scan() {
+		moduleMass, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Error("An error occured while converting the line of the file from string to int")
+		}
+
+		total += calcSecond(moduleMass)
+	}
+
+	return total
+}
+
+func calcSecond(mass int) int {
+	inMassFuel := calculateFuel(mass)
+	fuelForFuel := calculateFuel(inMassFuel)
+
+	result := inMassFuel + fuelForFuel
+	for {
+		fuel := calculateFuel(fuelForFuel)
+
+		if fuel <= 0 {
+			break
+		}
+		fuelForFuel = fuel
+		result += fuel
+	}
+
+	return result
+
+}
