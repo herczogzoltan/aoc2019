b29e0ee1d55b99d5debf7f9891350a913d6b928d
Zoltán Herczog <zoltan.herczog@pm.me>
2019-12-11 23:02:44 +0100

    Add Day 2 part 2 solution and refactor part 1 solution

--- cmd/day2.go
+++ cmd/day2.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"bufio"
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
 
@@ -15,46 +16,37 @@ var (
 		Run: func(cmd *cobra.Command, args []string) {
 			switch Part {
 			case 1:
-				fmt.Println("First part result: ", gravityAssist())
-				// case 2:
-				// 	fmt.Println("Second part result: ", secondPart())
+				fmt.Println("First part result: ", gravityAssist(12, 2))
+			case 2:
+				fmt.Printf("Second part result: %v", instructionPointer())
 			}
 		},
 	}
+
+	desiredOutput = 19690720
 )
 
 func init() {
 	rootCmd.AddCommand(day2)
 }
 
-func gravityAssist() int {
+func gravityAssist(noun int, verb int) int {
 	file := LoadSourceFile(InputSource)
 	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	scanner.Scan()
-	firstLine := scanner.Text()
-	nums := strings.Split(firstLine, ",")
-
-	var numbers []int
-	for _, value := range nums {
-		num, err := strconv.Atoi(value)
-		Check(err)
-		numbers = append(numbers, num)
-	}
+	addresses := getRowAsNumberArray(file)
 
 	i := 0
 	// because 1202 program alarm: https://www.hq.nasa.gov/alsj/a11/a11.landing.html#1023832
-	numbers[1] = 12
-	numbers[2] = 2
-	for i != 99 && i < len(numbers) {
-		opCode := numbers[i]
+	addresses[1] = noun
+	addresses[2] = verb
+	for i < len(addresses) {
+		opCode := addresses[i]
 
 		switch opCode {
 		case 1:
-			numbers[numbers[i+3]] = numbers[numbers[i+1]] + numbers[numbers[i+2]]
+			addresses[addresses[i+3]] = addresses[addresses[i+1]] + addresses[addresses[i+2]]
 		case 2:
-			numbers[numbers[i+3]] = numbers[numbers[i+1]] * numbers[numbers[i+2]]
+			addresses[addresses[i+3]] = addresses[addresses[i+1]] * addresses[addresses[i+2]]
 		case 99:
 			break
 		}
@@ -62,5 +54,40 @@ func gravityAssist() int {
 		i += 4
 	}
 
-	return numbers[0]
+	return addresses[0]
+}
+
+func instructionPointer() int {
+	noun := 0
+	for noun <= 99 {
+		verb := 0
+		for verb <= 99 {
+			result := gravityAssist(noun, verb)
+
+			fmt.Printf("Verb: %v, Noun: %v Result: %v\n", verb, noun, result)
+			if result == desiredOutput {
+				return 100*noun + verb
+			}
+
+			verb++
+		}
+		noun++
+	}
+
+	return 0
+}
+
+func getRowAsNumberArray(file *os.File) []int {
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	firstLine := scanner.Text()
+	nums := strings.Split(firstLine, ",")
+
+	var addresses []int
+	for _, value := range nums {
+		num, err := strconv.Atoi(value)
+		Check(err)
+		addresses = append(addresses, num)
+	}
+	return addresses
 }
